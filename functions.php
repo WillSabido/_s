63622ec0a29f8d8f1ba1410a2a03159bba8de38e
Will Sabido <will@dev.webisabi.co.uk>
2014-04-09 00:30:09 +0100

    Enabled bootstrap JS & CSS in functions

--- functions.php
+++ functions.php
@@ -115,3 +115,37 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+/**
+ * Enable Bootstrap, jQuery, Font Awesome
+ */
+// Only on front-end pages, NOT in admin area
+if (!is_admin()) {
+	// Load CSS
+	add_action('wp_enqueue_scripts', 'twbs_load_styles', 11);
+	function twbs_load_styles() {
+		// Bootstrap
+		wp_register_style('bootstrap-styles', '//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css', array(), null, 'all');
+		wp_enqueue_style('bootstrap-styles');
+		// Bootswatch Amelia styles
+//		wp_register_style('bootswatch-styles', '//netdna.bootstrapcdn.com/bootswatch/3.1.0/amelia/bootstrap.min.css', array(), null, 'all');
+//		wp_enqueue_style('bootswatch-styles');
+		// Theme Styles
+//		wp_register_style('theme-styles', get_stylesheet_uri(), array(), null, 'all');
+//		wp_enqueue_style('theme-styles');
+		// Font Awesome
+		wp_register_style('font-awesome', '//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css', array(), null, 'all');
+		wp_enqueue_style('font-awesome');
+	}
+	// Load Javascript
+	add_action('wp_enqueue_scripts', 'twbs_load_scripts', 12);
+	function twbs_load_scripts() {
+		// jQuery
+		wp_deregister_script('jquery');
+		wp_register_script('jquery', '//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js', array(), null, false);
+		wp_enqueue_script('jquery');
+		// Bootstrap
+		wp_register_script('bootstrap-scripts', '//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js', array('jquery'), null, true);
+		wp_enqueue_script('bootstrap-scripts');
+	}
+
+} // end if !is_admin
