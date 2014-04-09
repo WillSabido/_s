a669de4971a979eeea9c98493b3e358c191b19af
Will Sabido <will@dev.webisabi.co.uk>
2014-04-09 01:41:50 +0100

    Slider path update and search element & site desc styling

--- slider.php
+++ slider.php
@@ -18,21 +18,21 @@
   <!-- Wrapper for slides -->
   <div class="carousel-inner">
     <div class="item active">
-      <img src="./fc-elements/uploads/2014/02/slide14-green.jpg" alt="slide1">
+      <img src="./fc-elements/slides/slide1.jpg" alt="slide1">
       <div class="carousel-caption">
         <h3>Our Mission</h3>
 	<p>To be a leading African investment bureau and be recognised as a trusted business partner for foreign business undertaking operations in Africa.</p>
       </div>
     </div>
     <div class="item">
-      <img src="./fc-elements/uploads/2014/02/slide15-blue.jpg" alt="slide2">
+      <img src="./fc-elements/slides/slide2.jpg" alt="slide2">
       <div class="carousel-caption">
         <h3>Our Associates</h3>
 	<p>Coming Soon - a section listing a number of African countries so that you are able to click directly to see the name of that locality's Associate firm and their respective office address and contact details.</p>
       </div>
     </div>
     <div class="item">
-      <img src="./fc-elements/uploads/2014/02/slide16-orange.jpg" alt="slide3">
+      <img src="./fc-elements/slides/slide3.jpg" alt="slide3">
       <div class="carousel-caption">
         <h3>Our Focus</h3>
 	<p>
@@ -44,14 +44,14 @@
       </div>
     </div>
     <div class="item">
-      <img src="./fc-elements/uploads/2014/02/slide3.png" alt="slide4">
+      <img src="./fc-elements/slides/slide4.jpg" alt="slide4">
       <div class="carousel-caption">
         <h3>Our approach</h3>
 	<p>We work with our Associates across Africa to provide a holistic top quality service.<br/>We can connect you to the right advisors in any location across Africa.</p>
       </div>
     </div>
     <div class="item">
-      <img src="./fc-elements/uploads/2014/02/slide6.jpg" alt="slide5">
+      <img src="./fc-elements/slides/slide5.jpg" alt="slide5">
       <div class="carousel-caption">
         <h3>Site under construction</h3>
 	<p>Content will be added in the near future. In the meantime if you'd like to find out a bit more about our services please get in touch to <a href="./contact"><button type="button" class="btn btn-default btn-lg">arrange a consultation</button></a></p>
